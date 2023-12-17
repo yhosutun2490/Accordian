32d8b675f77dc4aab6ab637e788b14f9d5e9ea27
yhosutun2491 <yhosutun2491@gmail.com>
2023-12-18 00:00:45 +0800

    <feat> 完成基本桌機排版和手風琴

--- accordian/src/App.vue
+++ accordian/src/App.vue
@@ -1,33 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <h1 class="text-3xl font-bold">
-    Hello world!
-  </h1>
+<container class="container">
+  <header class="header"></header>
+  <FaqCardVue class="card"/>
+</container>
 </template>
 
+<script setup>
+import FaqCardVue from "./components/FaqCard.vue";
+
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.container {
+  width: 100%;
+  height: 100%;
+  background-color: #F8EEFF;
+  margin: 0;
+  position: relative;
+  display: flex;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+.header {
+  background: url(./assets/images/background-pattern-desktop.svg);
+  width: 100%;
+  height: 35%;
+  background-repeat: no-repeat;
+  background-size: auto;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.card {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  min-height: 335px;
 }
 </style>
---
+++ accordian/src/components/FaqCard.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="card-container">
+    <div class="title-wrap">
+      <img class="title-img" :src="StarIcon" alt="star-icon"/>
+      <p class="title-word">FAQ</p>
+    </div>
+    <div class="card-row-wrap">
+      <div class="card-row" v-for="data in cardRowData" :key="data.id">
+        <div class="row-title-wrap">
+          <div>{{ data.title }}</div>
+          <button class="open-btn" v-if="data.isShowCard" @click.stop ="toggleCard(data.id)"></button>
+          <button class="close-btn" v-if="!data.isShowCard" @click.stop="toggleCard(data.id)"></button>
+        </div>
+        <div class="card-content" v-show="data.isShowCard">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et fugit cupiditate unde veniam minus sequi, vitae aperiam quibusdam provident similique eligendi consequatur, impedit deleniti modi adipisci necessitatibus quos dolores. Nam!</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+</template>
+<script setup>
+import StarIcon from "../assets/images/icon-star.svg"
+import {ref} from 'vue'
+const cardRowData = ref([
+  {
+    id: 1,
+    title: 'What is Frontend Mentor, and how will it help me?',
+    content: 'hello',
+    isShowCard: false,
+  },
+   {
+    id: 2,
+    title: 'What is Frontend Mentor, and how will it help me?',
+    content: 'hello',
+     isShowCard: false,
+  },
+   {
+    id: 3,
+    title: 'What is Frontend Mentor, and how will it help me?',
+    content: 'hello',
+     isShowCard: false,
+  },
+])
+
+function toggleCard (id) {
+  cardRowData.value = cardRowData.value.map(item=>{
+    if (item.id === id ) {
+      item.isShowCard =  !item.isShowCard
+    }  return item
+  })
+}
+</script>
+<style scoped lang="scss">
+.card-container {
+  width: 41.66%;
+  background-color: white;
+  border-radius: 16px;
+}
+.title-wrap {
+  display: flex;
+  align-items: center;
+  height: 66px;
+  .title-img {
+    width: 40px;
+    height: 40px;
+    margin-right: 24px;
+  }
+  .title-word {
+    font-family: 'Work Sans';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 56px;
+    line-height: 66px;
+    color: #301534;
+  }
+}
+.card-row:not(:last-child) { 
+    border-bottom: 1px solid #F8EEFF;
+  } 
+.row-title-wrap {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  height: 30px;
+  padding: 24px 0;
+  
+  
+  .open-btn,.close-btn {
+    all: unset;
+    margin-left: auto;
+    width: 24px;
+    height: 24px;
+    cursor: pointer;
+  }
+  .open-btn {
+    background-image: url(../assets/images/icon-plus.svg);
+    background-position: center;
+    background-size: contain;
+  }
+  .close-btn {
+    background-image: url(../assets/images/icon-minus.svg);
+    background-position: center;
+    background-size: contain;
+  }
+}
+
+</style>
